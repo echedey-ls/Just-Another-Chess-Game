aa359abd21c29f95045f77b67e604a2900ca47f0
echedey-ls <80125792+echedey-ls@users.noreply.github.com>
2023-04-26 01:19:15 +0200

    Añadir constructor de copia

--- src/Posicion.h
+++ src/Posicion.h
@@ -5,5 +5,6 @@ struct Posicion {
 	unsigned char x, y;
 	bool operator==(const Posicion& other) { return x == other.x and y == other.y; }
 	Posicion(unsigned char x_, unsigned char y_) : x{ x_ }, y{ y_ } {};
+	Posicion(const Posicion& other) noexcept : x{ other.x }, y{ other.y } {};
 	Posicion(Posicion&& other) noexcept : x{ other.x }, y{ other.y } {};
 };
