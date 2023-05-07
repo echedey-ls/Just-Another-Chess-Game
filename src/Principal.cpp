27a9c34da3ab1e320d8d583ae5618895845f7303
echedey-ls <80125792+echedey-ls@users.noreply.github.com>
2023-05-07 21:47:01 +0200

    Cambiar nombre programa

--- src/Principal.cpp
+++ src/Principal.cpp
@@ -24,7 +24,7 @@ int main(int argc, char* argv[])
 	glutInit(&argc, argv);
 	glutInitWindowSize(800, 600);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-	glutCreateWindow("MiJuego");
+	glutCreateWindow("Just Another Chess Game");
 
 	//habilitar luces y definir perspectiva
 	glEnable(GL_LIGHT0);
