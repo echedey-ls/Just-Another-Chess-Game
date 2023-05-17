93a179f54a013593742fe52f91be1237b9d174cb
echedey-ls <80125792+echedey-ls@users.noreply.github.com>
2023-05-17 04:53:02 +0200

    Añadir clase abstracta para botón o clickeable

--- src/interfaz_usuario/BotonUI.h
+++ src/interfaz_usuario/BotonUI.h
@@ -1,24 +1,21 @@
 #pragma once
 
-#include "freeglut.h"
+#include "Boton_generico.h"
 
 #include <functional>
 
-class BotonUI {
+class BotonUI: public Boton_generico {
 private:
-	float x_lo_bound, y_lo_bound, x_hi_bound, y_hi_bound, x_text, y_text, scale_text;
+	float x_text, y_text, scale_text;
 	const unsigned char* text;
-	std::function<void()> click_callback;
 public:
 	BotonUI(const unsigned char* txt, float x_txt, float y_txt, float scale_txt,
 		float x_lo, float y_lo, float x_hi, float y_hi)
-		: click_callback{ nullptr }, text{ txt }, x_text{ x_txt }, y_text{ y_txt }, scale_text{ scale_txt },
-		x_lo_bound{ x_lo }, y_lo_bound{ y_lo }, x_hi_bound{ x_hi }, y_hi_bound{ y_hi } {
+		: text{ txt }, x_text{ x_txt }, y_text{ y_txt }, scale_text{ scale_txt },
+		Boton_generico(x_lo, y_lo, x_hi, y_hi) {
 		// A partir de la cadena de texto se podrían deducir varias variables,
 		// pero habría que comprobar muchas propiedades del renderizado.
 		// TODO: maybe?
 	}
 	void ilustra();
-	void mouse(int button, int state, GLdouble x, GLdouble y);
-	void register_on_click(std::function<void()>func) { click_callback = func; }
 };
---
+++ src/interfaz_usuario/Boton_generico.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <functional>
+
+class Boton_generico {
+protected:
+	float x_lo_bound, y_lo_bound, x_hi_bound, y_hi_bound;
+	std::function<void()> click_callback;
+	bool _hovered;
+	// El click se define como presionado dentro y después soltado, también dentro
+	int _last_state;
+public:
+	Boton_generico(float x_lo, float y_lo, float x_hi, float y_hi)
+		: click_callback{ nullptr }, _hovered { false }, _last_state { -1 },
+		x_lo_bound{ x_lo }, y_lo_bound{ y_lo }, x_hi_bound{ x_hi }, y_hi_bound{ y_hi } {}
+	void mouse(int button, int state, double x, double y);
+	void register_on_click(std::function<void()>func) { click_callback = func; }
+	bool is_hovered() { return _hovered; }
+};
+
