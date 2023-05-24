aabe92ab712778e62afb460fa940d4a4cbcf3929
echedey-ls <80125792+echedey-ls@users.noreply.github.com>
2023-05-24 23:55:23 +0200

    Ups

--- src/Mundo.cpp
+++ src/Mundo.cpp
@@ -38,7 +38,7 @@ void Mundo::mouse(int button, int state, int x, int y) {
 	GLint viewport[4];
 	GLdouble modelview[16];
 	GLdouble projection[16];
-	GLfloat winX, winY; //, winZ;
+	GLfloat winX, winY, winZ;
 	GLdouble posX, posY, posZ;
 
 	glGetDoublev(GL_MODELVIEW_MATRIX, modelview);
@@ -49,11 +49,11 @@ void Mundo::mouse(int button, int state, int x, int y) {
 	winY = (float)viewport[3] - (float)y;
 	// Queremos la coordenada del plano con z = 0.0
 	// Así que obviamos obtener la profundidad de renderizado, y dejamos la que nos devolvería
-	//glReadPixels(x, int(winY), 1, 1, GL_DEPTH_COMPONENT, GL_FLOAT, &winZ);
+	glReadPixels(x, int(winY), 1, 1, GL_DEPTH_COMPONENT, GL_FLOAT, &winZ);
 	gluUnProject(winX, winY, 1.0, modelview, projection, viewport, &posX, &posY, &posZ);
 
 	GLdouble posX_r, posY_r, posZ_r;
-	gluUnProject(winX, winY, 1.0, modelview, projection, viewport, &posX_r, &posY_r, &posZ_r);
+	gluUnProject(winX, winY, winZ, modelview, projection, viewport, &posX_r, &posY_r, &posZ_r);
 
 	if (state == GLUT_UP) {
 		std::cout << "R^2 (Z := 0) x = " << posX << ", y = " << posY << ", z" << posZ << std::endl;
