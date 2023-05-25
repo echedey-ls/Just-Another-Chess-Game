c8d3af5d5ba4587c1ae5fd25667e396423c2293f
Celia-t22 <80168073+Celia-t22@users.noreply.github.com>
2023-05-26 00:34:20 +0200

    use the force

--- src/interfaz_usuario/Interfaz_Partida.h
+++ src/interfaz_usuario/Interfaz_Partida.h
@@ -9,7 +9,7 @@
 #include <vector>
 
 class Interfaz_Partida {
-	BotonUI btn_cambio_graficos{ (unsigned char*)"Cambiar\ntema\ngrafico",
+	BotonUI btn_cambio_graficos{ (unsigned char*)"USE\nTHE\nFORCE",
 		69.f, 10.f, 0.02f, 68.5f, 2.7f, 79.5f, 13.f};
 	//BotonUI tablero_click{ 0.0f,0.0f,64.0f,64.0f };
 
